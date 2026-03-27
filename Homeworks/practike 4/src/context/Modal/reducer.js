b78f799dd5930c117163e1c2fe56bec272b0619f
DildoraIkromjonova <ikromjonovadildora1@gmail.com>
2026-03-27 23:05:12 +0500

    changed

--- Homeworks/practike 4/src/context/Modal/reducer.js
+++
@@ -1,7 +0,0 @@
-export const reducer = (state, { type, payload }) => {
-  switch (type) {
-    case "close": return { ...state, type: false, payload };
-    case "open": return { ...state, type: true, payload };
-    default: return { ...state };
-  }
-}
--- Homeworks/practike 4/src/root/index.jsx
+++
@@ -1,7 +0,0 @@
-import Dashboard from "../components/Dashboard";
-
-const Root = () => {
-  return <Dashboard />
-}
-
-export default Root;
